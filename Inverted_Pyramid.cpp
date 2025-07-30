9f1970d1507af3b474e2b83a31247824302c4e6b
Aditya Sinha <sinha.aditya.btech2024@sitpune.edu.in>
2025-07-30 02:21:07 -0400

    Experiment 6

---
+++ Inverted_Pyramid.cpp
@@ -0,0 +1,36 @@
+// Name: Aditya Sinha 
+// PRN: 24070123006
+//Batch: EnTc A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 5;
+
+    for (int i = rows; i >= 1; i--) {
+       
+        for (int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+
+        
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/* OUTPUT
+
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
